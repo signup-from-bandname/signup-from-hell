aef50425cdce0690a79c65d62177bc8d4f1f3e22
Erik Aderhold <erik.aderhold@sevenval.com>
2017-06-30 01:34:50 +0200

    update

--- assets/js/captchas.js
+++ assets/js/captchas.js
@@ -1,7 +1,18 @@
-var captchas = {
+var captchaDB = {
   'wordsWithNumbers' : [
     '4nim4l',
     'c0mput3r',
     'p33psh0w',
+    'clubm4t3',
+    'w0odh4ck',
+    'ke3pc4lm',
+    'h4ckmac',
+    'g3ttingb0red',
+    'almo5td0ne',
+  ],
+  'succeed_at_last' : [
+    'youcangetitifyoureallywant',
+    'butyoumusttrytryandtrytryandtry',
+    'youwillsucceedatlast',
   ],
 };
